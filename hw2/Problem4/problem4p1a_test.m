49aefa36a40f9fda53eeb6744810ac539d6ed781
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2019-06-15 23:31:32 -0400

    rename test file

--- hw2/Problem4/problem4p1a_test.m
+++ hw2/Problem4/problem4p1a_test.m
@@ -1,3 +1,5 @@
+%% This file calculates the probaility of every 
+
 n_pts = 4096;
 pXarr = zeros(1, n_pts);
 for assignment = 0:n_pts-1
@@ -5,6 +7,8 @@ for assignment = 0:n_pts-1
     asgn_bin = [zeros(1, 12-length(asgn_bin)) asgn_bin];
     pXarr(assignment+1) = problem4p1a_joint(asgn_bin);
 end
+
+
 
 
 
